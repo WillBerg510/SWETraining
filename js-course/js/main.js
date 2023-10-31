5db468eb0e30f845ebd821e7105a0961acf1ecad
Will Berg <waberg@cfl.rr.com>
2023-10-30 20:02:04 -0400

    Commits JS 7

--- js-course/js/main.js
+++ js-course/js/main.js
@@ -1,3 +1,74 @@
+// Rock Paper Scissors
+playerOne = "paper";
+computer = "paper";
+if (playerOne === computer) {
+  console.log("Tie!");
+}
+else if (playerOne === "rock") {
+  if (computer === "paper") {
+    console.log("CPU wins!");
+  }
+  else {
+    console.log("Player wins!")
+  }
+}
+else if (playerOne === "paper") {
+  if (computer === "rock") {
+    console.log("Player wins!");
+  }
+  else {
+    console.log("CPU wins!");
+  }
+}
+else {
+  if (computer === "rock") {
+    console.log("CPU wins!");
+  }
+  else {
+    console.log("Player wins!");
+  }
+}
+
+let testScore = 59;
+let collegeStudent = false;
+let grade;
+if (testScore >= 90) {
+  grade = "A";
+}
+else if (testScore >= 80) {
+  grade = "B";
+}
+else if (testScore >= 70) {
+  grade = "C";
+}
+else if (testScore >= 60) {
+  grade = "D";
+}
+else {
+  if (collegeStudent) {
+    grade = "U";
+  }
+  else {
+    grade = "F";
+  }
+}
+console.log(grade);
+
+let soup = "chicken noodle soup";
+let crackers = true;
+let customerIsBanned = false;
+let reply;
+if (customerIsBanned) {
+  reply = "No soup for you!";
+} else if (soup && crackers) {
+  reply = `Here's your order of ${soup} and crackers.`;
+} else if (soup) {
+  reply = `Here's your order of ${soup}.`;
+} else {
+  reply = `Sorry, we're out of soup.`;
+}
+console.log(reply);
+
 // Code Challenge #1
 const myName = "Will";
 console.log(myName.charAt(Math.floor(Math.random() * myName.length)));
