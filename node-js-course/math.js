93aeeeb388e6b81313c6ee77a66043310d6752ec
Will Berg <waberg@cfl.rr.com>
2023-11-17 12:10:54 -0500

    Commits Node.js 1

---
+++ node-js-course/math.js
@@ -0,0 +1,17 @@
+exports.add = function add(a, b) {
+  return a + b;
+}
+
+exports.subtract = function subtract(a, b) {
+  return a - b;
+}
+
+exports.multiply = function multiply(a, b) {
+  return a * b;
+}
+
+exports.divide = function divide(a, b) {
+  return a / b;
+}
+
+//module.exports = {add, subtract, multiply, divide};
