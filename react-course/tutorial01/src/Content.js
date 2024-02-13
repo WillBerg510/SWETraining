4eaee171c7ec1abb81a0d7a5cfc26e8255828385
Will Berg <waberg@cfl.rr.com>
2024-02-12 20:57:13 -0500

    React up to 6

--- react-course/tutorial01/src/Content.js
+++ react-course/tutorial01/src/Content.js
@@ -1,19 +1,42 @@
-import logo from './logo.svg';
 import React from 'react'
+import {useState} from 'react'
 
 const Content = () => {
+  const [name, setName] = useState('Will');
+  const [count, setCount] = useState(0);
   const handleNameChange = function() {
     const names = ['Will', 'Lam', 'Sabrina', 'Brianna', 'Izzy'];
     const int = Math.floor(Math.random() * names.length);
-    return names[int];
+    setName(names[int]);
   }
-  
+
+  const handleClick = function() {
+    setCount(count + 1);
+    console.log(count + 1);
+  }
+
+  const handleClick2 = function() {
+    console.log(name + ' double clicked')
+  }
+
+  const handleClick3 = function(e) {
+    console.log(count)
+  }
+
   return (
     <main>
-      <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello, {handleNameChange()}!
+        <p onDoubleClick={handleClick2}>
+          Hello, {name}!
         </p>
+        <button onClick={handleNameChange}>
+          Click
+        </button>
+        <button onClick={handleClick}>
+          Click
+        </button>
+        <button onClick={handleClick3}>
+          Click
+        </button>
     </main>
   )
 }
