eb1a1660a93d1be74d884a71cc74404ee2d9f236
Will Berg <waberg@cfl.rr.com>
2024-03-01 11:29:33 -0500

    React up to 19

--- react-course/tutorial16/src/App.js
+++ react-course/tutorial16/src/App.js
@@ -4,66 +4,86 @@ import Footer from './Footer';
 import Home from './Home';
 import NewPost from './NewPost';
 import PostPage from './PostPage';
+import EditPost from './EditPost';
 import About from './About';
 import Missing from './Missing';
 import {Route, Switch, useHistory} from 'react-router-dom';
 import {useState, useEffect} from 'react';
 import {format} from 'date-fns';
+import api from './api/posts';
 
 function App() {
-  const [posts, setPosts] = useState([
-    {
-      id: 1,
-      title: "What's up, everyone?",
-      datetime: "December 14, 2023 04:23:22 PM",
-      body: "I'm excited to get to know you all! Does anyone have any cool projects they're working on?"
-    },
-    {
-      id: 2,
-      title: "My 2nd Post",
-      datetime: "July 01, 2021 11:17:36 AM",
-      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
-    },
-    {
-      id: 3,
-      title: "My 3rd Post",
-      datetime: "July 01, 2021 11:17:36 AM",
-      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
-    },
-    {
-      id: 4,
-      title: "My Fourth Post",
-      datetime: "July 01, 2021 11:17:36 AM",
-      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
-    }
-  ]);
+  const [posts, setPosts] = useState([]);
   const [search, setSearch] = useState('');
   const [searchResults, setSearchResults] = useState([]);
   const [postTitle, setPostTitle] = useState('');
   const [postBody, setPostBody] = useState('');
+  const [editTitle, setEditTitle] = useState('');
+  const [editBody, setEditBody] = useState('');
   const history = useHistory();
+
+  useEffect(() => {
+    const fetchPosts = async function() {
+      try {
+        const response = await api.get('/posts');
+        setPosts(response.data);
+      }
+      catch (err) {
+        if (err.response) console.log(err.response.data);
+        else console.log('Error: ' + err.message);
+      }
+    }
+    fetchPosts();
+  }, [])
 
   useEffect(() => {
     const filteredResults = posts.filter(post => ((post.body).toLowerCase()).includes(search.toLowerCase()) || ((post.title).toLowerCase()).includes(search.toLowerCase()));
     setSearchResults(filteredResults.reverse());
   },[posts, search])
 
-  const handleSubmit = function(e) {
+  const handleSubmit = async function(e) {
     e.preventDefault();
-    const id = posts.length ? posts[posts.length - 1].id + 1 : 1;
+    const id = posts.length ? Number(posts[posts.length - 1].id) + 1 : 1;
     const datetime = format(new Date(), 'MMMM dd, yyyy pp');
     const newPost = {id, title: postTitle, datetime, body: postBody};
-    const postsList = [...posts, newPost];
-    setPosts(postsList);
-    setPostTitle('');
-    setPostBody('');
-    history.push('/');
+    try {
+      const response = await api.post('/posts', newPost);
+      const postsList = [...posts, response.data];
+      setPosts(postsList);
+      setPostTitle('');
+      setPostBody('');
+      history.push('/');
+    }
+    catch (err) {
+      console.log('Error: ' + err.message);
+    }
   }
 
-  const handleDelete = function(id) {
-    const postsList = posts.filter(post => post.id !== id);
-    setPosts(postsList);
-    history.push('/')
+  const handleEdit = async function(id) {
+    const datetime = format(new Date(), 'MMMM dd, yyyy pp');
+    const updatedPost = {id, title: editTitle, datetime, body: editBody};
+    try {
+      const response = await api.put('/posts/' + id, updatedPost);
+      setPosts(posts.map(post => post.id === id ? {...response.data} : post));
+      setEditTitle('');
+      setEditBody('');
+      history.push('/');
+    }
+    catch (err) {
+      console.log('Error: ' + err.message);
+    }
+  }
+
+  const handleDelete = async function(id) {
+    try {
+      await api.delete('/posts/' + id);
+      const postsList = posts.filter(post => post.id !== id);
+      setPosts(postsList);
+      history.push('/')
+    }
+    catch (err) {
+      console.log('Error: ' + err.message);
+    }
   }
 
   return (
@@ -76,6 +96,9 @@ function App() {
         </Route>
         <Route exact path="/post">
           <NewPost handleSubmit={handleSubmit} postTitle={postTitle} setPostTitle={setPostTitle} postBody={postBody} setPostBody={setPostBody}/>
+        </Route>
+        <Route path="/edit/:id">
+          <EditPost posts={posts} handleEdit={handleEdit} editTitle={editTitle} setEditTitle={setEditTitle} editBody={editBody} setEditBody={setEditBody}/>
         </Route>
         <Route path="/post/:id">
           <PostPage posts={posts} handleDelete={handleDelete}/>
